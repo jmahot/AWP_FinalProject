a0fb69ad25aff594eab78f6e6a4fcbf2cd05e33e
Julie MAHOT <julie.mahot@outlook.fr>
2023-12-05 02:46:36 +0100

    new changes - debug restart from the beginning

--- e-commerce/vue.config.js
+++ e-commerce/vue.config.js
@@ -1,4 +1,13 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+const path = require('path');
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  outputDir: 'dist',
+  assetsDir: 'frontend',
+  indexPath: 'frontend/index.html',
+  chainWebpack: config => {
+    config.resolve.alias.set('@', path.resolve(__dirname, 'frontend'));
+    config.entry('app').clear().add(path.resolve(__dirname, 'frontend/main.js')).end();
+  }
+});
